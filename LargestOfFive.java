3b4f11aaba63ccd99789cb4066c36b9a21ded05a
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-24 02:40:47 -0400

    Update LargestOfFive.java

--- LargestOfFive.java
+++ LargestOfFive.java
@@ -12,7 +12,6 @@
 public class LargestOfFive {
 
     public static void main (String[] args) {
-
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
